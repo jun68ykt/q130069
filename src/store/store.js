0262795673a8cee5dc460f5958de72e24039d7c9
Junichi Yokota <jun68.yokota@gmail.com>
2018-06-08 16:56:30 +0900

    redux-logger追加

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
 export default function configureStore() {
   return createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, createLogger())
   );
 };
